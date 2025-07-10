49dad07d6b462fca3a76165861b8007ebb2829d3
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-07-10 23:15:38 +0500

    javaScript course

---
+++ Chapter 01/variable.js
@@ -0,0 +1,11 @@
+// name = "hamza";
+// age = 22;
+// price = 100.5
+// x = null;
+// z = undefined
+// isFollow = true;
+// console.log(name, age, price)
+// console.log(x, z);
+// console.log(isFollow)
+
+//  Time    41:00
