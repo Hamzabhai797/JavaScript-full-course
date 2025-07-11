b8564794f5d6786736050b291773783b87e565a2
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-07-11 19:12:13 +0500

    data types

---
+++ Chapter 01/datatypes.js
@@ -0,0 +1,10 @@
+let age = 22  // Datatype number
+let name = "hamza"  // Datatype string
+let price = 67.98  // Datatype number
+let x = null  // Datatype null
+let z = undefined  // Datatype undefined
+let isFollow = true  // Datatype boolean
+console.log(name, age, price);  // Output: hamza 22 67.98
+console.log(x, z);  // Output: null undefined
+
+// time 1:6:00
--- Chapter 01/variable.js
+++ Chapter 01/variable.js
@@ -8,4 +8,18 @@
 // console.log(x, z);
 // console.log(isFollow)
 
-//  Time    41:00
+// let name = "hamza"
+// let age = 22;
+// let price = 67.98
+// console.log(name, age)
+
+                   // var
+// var age = 22
+// var age = 23
+// var age = 24
+// console.log(age)
+
+// let age = 22
+// let age = 23
+// let age = 24
+// console.log(age)
