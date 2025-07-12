83b151384889d3870cadb3fc1f071dd5af06a44d
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-07-12 21:09:07 +0500

    chapter 03

--- Chapter 01/datatypes.js
+++ Chapter 01/datatypes.js
@@ -1,10 +1,28 @@
-let age = 22  // Datatype number
-let name = "hamza"  // Datatype string
-let price = 67.98  // Datatype number
-let x = null  // Datatype null
-let z = undefined  // Datatype undefined
-let isFollow = true  // Datatype boolean
-console.log(name, age, price);  // Output: hamza 22 67.98
-console.log(x, z);  // Output: null undefined
+// let age = 22  // Datatype number
+// let name = "hamza"  // Datatype string
+// let price = 67.98  // Datatype number
+// let x = null  // Datatype null
+// let z = undefined  // Datatype undefined
+// let isFollow = true  // Datatype boolean
+// console.log(name, age, price);  // Output: hamza 22 67.98
+// console.log(x, z);  // Output: null undefined
 
-// time 1:6:00
+// const student = {
+//     name: "Hamza",
+//     age: 22,
+//     grade: "A"
+// }
+
+// student["name" = "Bilal"]
+// console.log(student.grade); 
+
+const product = {
+    title: "ball pen",
+    rating: 4.5,
+    offer: null,
+    isAvailable: true,
+}
+console.log(product)
+console.log(typeof product["title"])
+console.log(typeof product.isAvailable)
+// time 1:21:00
---
+++ Chapter 02/script.js
@@ -0,0 +1,21 @@
+// Arithmetic Opertors
+// let a = 5;
+// let b = 2;
+// console.log(a + b); // Addition
+// console.log(a - b); // Subtraction
+// console.log(a * b); // Multiplication
+// console.log(a / b); // Division
+// console.log(a % b); // Modulus
+// console.log(a ** b); // Exponentiation
+
+
+// unary Operators
+// let c = 5;
+// c = c+1; // Increment
+// console.log(c); // Increment
+
+// let c = 5;
+// c = c-1; // Decrement
+// console.log(c); // Decrement
+
+// time 1:38:00
