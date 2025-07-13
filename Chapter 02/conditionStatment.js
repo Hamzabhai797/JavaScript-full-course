5c3c72fb7949105cbb821aec6a1610f1a671b430
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-07-13 19:14:04 +0500

    learn prompt & alert

---
+++ Chapter 02/conditionStatment.js
@@ -0,0 +1,14 @@
+//conditional statement
+// let age = 20;
+// if (age >= 18) {
+//     console.log("You are eligible to vote.");
+// } 
+// else
+//     {
+//     console.log("You are not eligible to vote.");
+// }
+
+alert("Welcome to the voting system!");
+let age = prompt("Please enter your age:");
+
+// time = 2:20:00
