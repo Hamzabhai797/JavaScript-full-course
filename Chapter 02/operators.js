8585b3217f272ce97c664910142de377c409b597
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-07-13 18:44:44 +0500

    chapter 02 practice

---
+++ Chapter 02/operators.js
@@ -0,0 +1,49 @@
+// Arithmetic Opertors
+// let a = 5;
+// let b = 2;
+// console.log(a + b); // Addition
+// console.log(a - b); // Subtraction
+// console.log(a * b); // Multiplication
+// console.log(a / b); // Division
+// console.log(a % b); // Modulus
+// console.log(a ** b); // Exponentiation
+
+
+// unary Operators
+// let c = 5;
+// c = c+1; // Increment
+// console.log(c); // Increment
+
+// let c = 5;
+// c = c-1; // Decrement
+// console.log(c); // Decrement
+
+//Assignment Operators
+// let a = 5;
+// let b = 2;
+// a += b; // Addition Assignment
+// a -= b; // Subtraction Assignment
+// a *= b; // Multiplication Assignment
+// a /= b; // Division Assignment
+// a %= b; // Modulus Assignment
+// a **= b; // Exponentiation Assignment
+// console.log(a);
+
+// Comparison Operators
+// let a = 5;
+// let b = 2;
+// console.log(a == b); // Equal to
+// console.log(a === b);
+// console.log(a != b); // Not equal to
+// console.log(a !== b);
+// console.log(a > b); // Greater than
+// console.log(a < b); // Less than
+// console.log(a >= b); // Greater than or equal to
+// console.log(a <= b); // Less than or equal to
+
+// Logical Operators
+// let a = true;
+// let b = false;
+// console.log(a && b); // Logical AND
+// console.log(a || b); // Logical OR
+// console.log(!a); // Logical NOT
