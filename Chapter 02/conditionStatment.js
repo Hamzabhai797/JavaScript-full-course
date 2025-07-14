c115248f5823ac52db7221fce551bd4c65d83007
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-07-14 17:20:45 +0500

    chp 03

--- Chapter 02/conditionStatment.js
+++ Chapter 02/conditionStatment.js
@@ -8,7 +8,36 @@
 //     console.log("You are not eligible to vote.");
 // }
 
-alert("Welcome to the voting system!");
-let age = prompt("Please enter your age:");
+// alert("Welcome to the voting system!");
+// let age = prompt("Please enter your age:");
+
+// let number = prompt("Enter a number: ");
+// if (number > 0) {
+//     console.log("this is a positive number");
+// } else if (number < 0) {
+//     console.log("This is negative number")
+// } else {
+//     console.log("This is zero")
+// }
+
+// let number = prompt("Enter a number: ");
+// if (number % 5 === 0) {
+//     console.log("This is a multiple of 5");
+// } else {
+//     console.log("This is not a multiple of 5");
+// }
+
+let num = prompt("Enter a number: ");
+if (num >= 80 && num <= 100) {
+    console.log("You got an A grade");
+} else if (num >= 70 && num <= 89){
+    console.log("You got a B grade");
+} else if (num >= 60 && num <= 69) {
+    console.log("You got a C grade");
+} else if (num >= 50 && num <= 59) {
+    console.log("You got a D grade");
+} else {
+    console.log("You got an F grade");
+}
 
 // time = 2:20:00
---
+++ chapter 03/loops.js
@@ -0,0 +1,9 @@
+// for(let i = 1; i <= 5; i++){
+//     console.log("Hello Hamza")
+// }
+
+for(let i = 1; i <= 10; i++){
+    console.log("Hello Hamza")
+}
+
+// time 2:35:00
