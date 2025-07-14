e0b67517889e8090a7a96b7021776adc4ab40bb5
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-07-14 18:57:57 +0500

    for loop complete

--- chapter 03/loops.js
+++ chapter 03/loops.js
@@ -2,8 +2,23 @@
 //     console.log("Hello Hamza")
 // }
 
-for(let i = 1; i <= 10; i++){
-    console.log("Hello Hamza")
-}
+// for(let i = 1; i <= 10; i++){
+//     console.log("Hello Hamza")
+// }
+
+// for(let i = 1; i <= 100; i++) {
+//     console.log("Hello Hamza " + i);
+// }
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log("Hello Hamza ");
+// }
+// console.log("Loop finished");
+
+// let sum = 0;
+// for(i = 1; i<= 10; i++) {
+//     sum = sum + i
+// }
+// console.log(sum)
 
 // time 2:35:00
